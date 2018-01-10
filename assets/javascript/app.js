8721ac5ae37d2de18f891248ee5a817fcaafbc4e
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2018-01-10 13:14:24 -0500

    added form and table code to html, added firebase code on js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,5 +8,51 @@ var config = {
     storageBucket: "bootcamp-d0dc7.appspot.com",
     messagingSenderId: "679568646175"
 };
-
 firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+// var currentTime = moment();
+
+// Initial values
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var trainFrequency = "";
+
+// Capture Button Click
+$("#add-train").on("click", function () {
+    event.preventDefault();
+
+    trainName = $("#trainName-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    trainTime = $("#time-input").val().trim();
+    trainFrequency = $("#frequency-input").val().trim();
+
+    database.ref().set({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        trainFrequency: trainFrequency
+    });
+});
+
+database.ref().on("value", function (snapshot) {
+    console.log(snapshot.val());
+    console.log(snapshot.val().trainName);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().trainTime);
+    console.log(snapshot.val().trainFrequency);
+
+    // Change the HTML to reflect
+    $("#trainName-display").text(snapshot.val().trainName);
+    $("#destination-display").text(snapshot.val().destination);
+    $("#trainTime-display").text(snapshot.val().trainTime);
+    $("#frequency-display").text(snapshot.val().trainFrequency);
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+
+})
+
