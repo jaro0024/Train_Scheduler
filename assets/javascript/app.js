3d0172c4e3b236f6d3030b0368d2b09246b5986e
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2018-01-10 11:01:48 -0500

    added basic code for html and css, and added code to initialize firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA-xVk3PolrpsTRTQSPzATxEQZO3_VBp-A",
+    authDomain: "bootcamp-d0dc7.firebaseapp.com",
+    databaseURL: "https://bootcamp-d0dc7.firebaseio.com",
+    projectId: "bootcamp-d0dc7",
+    storageBucket: "bootcamp-d0dc7.appspot.com",
+    messagingSenderId: "679568646175"
+};
+
+firebase.initializeApp(config);
